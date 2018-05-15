64a74898f85d02cca4d7fac4d642783b37120451
guildeassis <guildeassis@gmail.com>
2018-05-15 13:37:16 -0300

    Tudo pronto

--- app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
@@ -3,17 +3,20 @@ package br.ufmg.coltec.tp.e04intents;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.Paint;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-//    Codigo da foto de perfil
-    private static int FOTO_CODE = 1;
+    private static int FOTO_CODE = 1;   //Codigo da foto de perfil
     ImageButton imgButton;
 
     @Override
@@ -21,6 +24,14 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        TextView Telefone = findViewById(R.id.Telefone);
+        Telefone.setPaintFlags(Telefone.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);//Sublinhar o texto
+
+        TextView email = findViewById(R.id.email);
+        email.setPaintFlags(email.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);//Sublinhar o texto
+
+        //Foto do perfil:
         imgButton =(ImageButton)findViewById(R.id.imageButton);
         imgButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +40,30 @@ public class MainActivity extends Activity {
                 startActivityForResult(fotoIntent, FOTO_CODE);
             }
         });
+
+
+        //email
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("mailto:gui@g.com");
+                Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
+                startActivity(intent);
+            }
+        });
+
+
+        //Telefone:
+        Telefone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("tel:999999999");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+
+
 
     }
 
