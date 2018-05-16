27fbfb9c31a9f71126fa4e4b56b5f5149a49df12
RitaRez <ritarezborges@gmail.com>
2018-05-16 20:06:54 -0300

    Adicao de exercicios de intent

--- app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
@@ -1,13 +1,79 @@
 package br.ufmg.coltec.tp.e04intents;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+
+    private static int FOTO_CODE = 1;
+    TextView tel;
+    TextView email;
+    ImageView foto;
+    ImageButton fotoBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tel = findViewById(R.id.telefone);
+        email = findViewById(R.id.email);
+        foto = findViewById(R.id.foto);
+        fotoBtn = findViewById(R.id.imageButton);
+
+        tel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Uri uri = Uri.parse("tel:" + tel.getText().toString());
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(Intent.ACTION_SEND);
+                i.setType("message/rfc822");
+                i.putExtra(Intent.EXTRA_EMAIL  , new String[]{tel.getText().toString()});
+                i.putExtra(Intent.EXTRA_SUBJECT, "subject of email");
+                i.putExtra(Intent.EXTRA_TEXT   , "body of email");
+                try {
+                    startActivity(Intent.createChooser(i, "Send mail..."));
+                } catch (android.content.ActivityNotFoundException ex) {
+                    Toast.makeText(MainActivity.this, "There are no email clients installed.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        fotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent fotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(fotoIntent, FOTO_CODE);            }
+        });
+
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        ImageView imageView = findViewById(R.id.foto);
+
+        // verifica se retorno pertence a Intent chamada anterioremente,
+        // e se ela foi executada corretamente
+        if (requestCode == FOTO_CODE && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(photo);
+        }
     }
 }
