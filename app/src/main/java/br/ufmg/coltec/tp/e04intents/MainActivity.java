b08882e2e31df7e604f725a799ac939a5fd14950
ibryans <bryans0701@gmail.com>
2018-05-24 10:04:37 -0300

    App inicial feito - Tirar foto, email e telefone

--- app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
@@ -1,13 +1,66 @@
 package br.ufmg.coltec.tp.e04intents;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity extends Activity {
+
+    private static int FOTO_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CircleImageView img = findViewById(R.id.perfil);
+        TextView email = findViewById(R.id.email);
+        TextView telefone = findViewById(R.id.telefone);
+
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent fotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(fotoIntent,FOTO_CODE);
+            }
+        });
+
+        email.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_SEND);
+                intent.setType("text/html");
+                intent.putExtra(Intent.EXTRA_EMAIL, "bryans0701@gmail.com");
+                startActivity(intent);
+            }
+        });
+
+        telefone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("tel:982310492");
+                Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        CircleImageView img = findViewById(R.id.perfil);
+
+        if (requestCode == FOTO_CODE && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            img.setImageBitmap(photo);
+        }
     }
 }
