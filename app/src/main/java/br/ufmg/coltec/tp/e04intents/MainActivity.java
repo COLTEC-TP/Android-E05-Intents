9f3f62a500f9008cd7a0fc4422945703c2353be2
kingnowdo <reinaldomagas@gmail.com>
2018-05-24 11:16:30 -0300

    Leitura de código de barra pronto

--- app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
@@ -7,12 +7,16 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    private static final int  FOTO_CODE = 1;
+    private static final int FOTO_CODE = 1;
+    private static final int BARRA_CODE = 2;
+    private static final String ACTION_CODBARRA = "com.google.zxing.client.android.SCAN";
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +25,7 @@ public class MainActivity extends Activity {
         ImageView foto = findViewById(R.id.perfil_foto);
         TextView texTelefone = findViewById(R.id.perfil_fone);
         TextView texEmail = findViewById(R.id.perfil_email);
+        Button botao = findViewById(R.id.botao);
 
         foto.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
@@ -47,16 +52,41 @@ public class MainActivity extends Activity {
             }
         });
 
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ACTION_CODBARRA);
+                try{
+                    startActivityForResult(intent, BARRA_CODE); // A B S U R D O !!!  O Android Studio não avisa que precisa de try catch
+                }
+                catch (android.content.ActivityNotFoundException e){
+                    Toast.makeText(MainActivity.this,"Você não tem aplicativo de leitura de código de barra",Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         ImageView imageView = findViewById(R.id.perfil_foto);
 
-        // verifica se retorno pertence a Intent chamada anterioremente e se ela foi executada corretamente
-        if (requestCode == FOTO_CODE && resultCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) data.getExtras().get("data");
-            imageView.setImageBitmap(photo);
+        switch (requestCode){
+            case FOTO_CODE:
+                if (resultCode == Activity.RESULT_OK) {
+                    Bitmap photo = (Bitmap) data.getExtras().get("data");
+                    imageView.setImageBitmap(photo);
+                }
+                break;
+            case BARRA_CODE:
+                if (resultCode == Activity.RESULT_OK) {
+                    int resultado = Integer.parseInt(data.getStringExtra("SCAN_RESULT"));
+                    Toast.makeText(MainActivity.this,"O código é " + resultado,Toast.LENGTH_LONG).show();
+                }
+                break;
+            default:
+                //nada
+                break;
         }
     }
 }
