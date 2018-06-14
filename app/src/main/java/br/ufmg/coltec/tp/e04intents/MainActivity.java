29c2a9de294342b3e75c2016fc071f939ea3349b
veazey <gabrielveazey7@gmail.com>
2018-06-14 15:21:51 -0300

    Exercicios finalizados e arrumados

--- app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
@@ -3,7 +3,6 @@ package br.ufmg.coltec.tp.e04intents;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.Paint;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
@@ -12,54 +11,76 @@ import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    private static int CODIGO = 1;
+    private static int CODE = 1;
     ImageButton imgButton;
+    private static final int CODIGODEBARRA_CODE = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TextView Tel = findViewById(R.id.Tel);
+
+
+        TextView Telefone = findViewById(R.id.Telefone);
+
         TextView email = findViewById(R.id.email);
 
-        imgButton =findViewById(R.id.imageButton);
+        Button botao = findViewById(R.id.botaoScanner);
+
+        imgButton =(ImageButton)findViewById(R.id.imageButton);
         imgButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent fotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(fotoIntent, CODIGO);
+                startActivityForResult(fotoIntent, CODE);
             }
         });
+
 
         email.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Uri uri = Uri.parse("gabrielveazey7@gmail.com");
+                Uri uri = Uri.parse("mailto:EXEMPLO@HOT.com");
                 Intent intent = new Intent(Intent.ACTION_SENDTO, uri);
                 startActivity(intent);
             }
         });
-        Tel.setOnClickListener(new View.OnClickListener() {
+
+
+        Telefone.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Uri uri = Uri.parse("031986750187");
+                Uri uri = Uri.parse("tel:123456789");
                 Intent intent = new Intent(Intent.ACTION_DIAL, uri);
                 startActivity(intent);
             }
         });
+
+
+        botao.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
+                startActivityForResult(intent, CODIGODEBARRA_CODE);
+            }
+        });
+
     }
 
     @Override
-    protected void onActivityResult(int PEGACode, int resultaCode, Intent info) {
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         ImageView imageView = findViewById(R.id.imageButton);
 
-        if (PEGACode == CODIGO && resultaCode == Activity.RESULT_OK) {
-            Bitmap photo = (Bitmap) info.getExtras().get("info");
+        if (requestCode == CODE && resultCode == Activity.RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
             imageView.setImageBitmap(photo);
+        }
+        if (requestCode == CODIGODEBARRA_CODE && resultCode == Activity.RESULT_OK) {
+            TextView imprimirCodigoDeBarras = findViewById(R.id.textoCodigoBarraas);
+            imprimirCodigoDeBarras.setText(data.getStringExtra("SCAN_RESULT"));
         }
     }
 }
