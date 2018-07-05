465b9e1c7fe90c25a89fc94b45520b24297468ff
ibryans <bryans0701@gmail.com>
2018-07-05 08:41:34 -0300

    Finalizado

--- app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
@@ -7,6 +7,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -23,8 +24,9 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         CircleImageView img = findViewById(R.id.perfil);
-        TextView email = findViewById(R.id.email);
+        final TextView email = findViewById(R.id.email);
         TextView telefone = findViewById(R.id.telefone);
+        Button btn = findViewById(R.id.btn_codigo);
 
         img.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,8 +40,9 @@ public class MainActivity extends Activity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(Intent.ACTION_SEND);
-                intent.setType("text/html");
-                intent.putExtra(Intent.EXTRA_EMAIL, "bryans0701@gmail.com");
+                intent.setType("message/rfc822");
+                intent.putExtra(Intent.EXTRA_EMAIL  , new String[]{"bryans0701@gmail.com"});
+                intent.putExtra(Intent.EXTRA_SUBJECT, "Bom dia jovem!");
                 startActivity(intent);
             }
         });
@@ -49,6 +52,14 @@ public class MainActivity extends Activity {
             public void onClick(View v) {
                 Uri uri = Uri.parse("tel:982310492");
                 Intent intent = new Intent(Intent.ACTION_DIAL, uri);
+                startActivity(intent);
+            }
+        });
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.google.zxing.client.android.SCAN");
                 startActivity(intent);
             }
         });
