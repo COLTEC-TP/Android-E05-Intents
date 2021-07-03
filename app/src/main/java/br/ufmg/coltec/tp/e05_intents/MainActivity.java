4ad5cc8437260c6f74fc147889a6ecdc85ef9a2f
Mateus3578 <76887902+Mateus3578@users.noreply.github.com>
2021-07-02 21:19:16 -0300

    Quase ok :)
    + Tela de perfil implementada com todas as funcionalidades.
    
    + Funcionalidade extra: ao trocar na foto para alterá-la, é possível escolher entre a câmera e a galeria
    
    + Integração com o app Barcode Scanner criada. Falta tratar o resultado corretamente

---
+++ app/src/main/java/br/ufmg/coltec/tp/e05_intents/MainActivity.java
@@ -0,0 +1,88 @@
+package br.ufmg.coltec.tp.e05_intents;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int BARCODE_CODE = 0;
+    private static final int CAMERA_CODE = 1;
+    private static final int GALLERY_CODE = 2;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            switch (requestCode){
+                case 0:
+                    String contents = data.getStringExtra("SCAN_RESULT");
+                    String format = data.getStringExtra("SCAN_RESULT_FORMAT");
+                    TextView barcode = findViewById(R.id.txt_barcode);
+                    barcode.setText(contents);
+                    break;
+                case 1:
+                case 2:
+                    ImageView image = findViewById(R.id.img_photo);
+                    Bitmap photo = data.getExtras().getParcelable("data");
+                    image.setImageBitmap(photo);
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageView image = findViewById(R.id.img_photo);
+        TextView name = findViewById(R.id.txt_name);
+        TextView age = findViewById(R.id.txt_age);
+        TextView phone = findViewById(R.id.txt_phone);
+        TextView email = findViewById(R.id.txt_email);
+        Button button = findViewById(R.id.btn_barcode);
+
+        name.setText("Gill Bates");
+        age.setText("35 anos");
+        email.setText("gillbates@macrosoft.com.br");
+        phone.setText("4002-8922");
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Escolha:");
+        builder.setItems(new String[]{"Camera", "Galeria"}, (dialog, which) -> {
+            switch (which) {
+                case 0:
+                    Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    startActivityForResult(cameraIntent, CAMERA_CODE);
+                    break;
+                case 1:
+                    Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(galleryIntent, GALLERY_CODE);
+                    break;
+                default:
+                    break;
+            }
+        });
+
+        image.setOnClickListener(v -> builder.show());
+        phone.setOnClickListener(v -> startActivity(new Intent(Intent.ACTION_DIAL, Uri.parse(phone.getText().toString()))));
+        email.setOnClickListener(v -> {
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse("mailto:" + email.getText().toString()));
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Email");
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "Olá!");
+            startActivity(emailIntent);
+        });
+        button.setOnClickListener(v -> startActivityForResult(new Intent("com.google.zxing.client.android.SCAN"), BARCODE_CODE));
+
+    }
+}
