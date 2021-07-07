50f73b4e4f224209f03e5c7d4d8e63f37cd8d953
DougNaves <61755699+DougNaves@users.noreply.github.com>
2021-07-07 20:27:31 -0300

    Entrega da atividade "leitura de código de barras" - Douglas Coelho

--- app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
+++ app/src/main/java/br/ufmg/coltec/tp/e04intents/MainActivity.java
@@ -2,67 +2,59 @@ package br.ufmg.coltec.tp.e04intents;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
+import android.widget.Toast;
 
-import java.security.PrivateKey;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+
 
 public class MainActivity extends Activity {
-    private static int IMG_CODE = 1;
+
+    private static int SCAN_CODE = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button editImage = findViewById(R.id.btn_img);
-        TextView userEmail = findViewById(R.id.user_email);
-        TextView userNumber = findViewById(R.id.user_number);
+        Button btnScan = findViewById(R.id.btn_scan);
 
-        editImage.setOnClickListener(new View.OnClickListener() {
+        btnScan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent editImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(editImage, 1);
+//                new IntentIntegrator(MainActivity.this).initiateScan();
+
+                Intent scanner = new Intent("com.google.zxing.client.android.SCAN");
+                startActivityForResult(scanner, SCAN_CODE);
             }
         });
-
-        userEmail.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Uri uri = Uri.parse("mailto:");
-                Intent email = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(email);
-            }
-        });
-
-        userNumber.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                TextView num = (TextView) v;
-                String userNum = num.getText().toString();
-                Uri uri = Uri.parse(new StringBuilder().append("tel:").append(userNum).toString());
-                Intent email = new Intent(Intent.ACTION_VIEW, uri);
-                startActivity(email);
-            }
-        });
-
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-        if(requestCode == IMG_CODE && resultCode == Activity.RESULT_OK){
-            Bitmap user = (Bitmap) data.getExtras().get("data");
-            ImageView userImage = findViewById(R.id.img_user);
-            userImage.setImageBitmap(user);
+        if (requestCode == SCAN_CODE && resultCode == Activity.RESULT_OK){
+
+            String cod = data.getStringExtra("SCAN_RESULT");
+//            Log.d("debug",cod);
+            Toast.makeText(this, "Codigo: " + cod, Toast.LENGTH_LONG).show();
+        }
+        else{
+            Toast.makeText(this, "Erro na leitura do código de barras", Toast.LENGTH_LONG).show();
         }
 
+//        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+//        if(result != null) {
+//            if(result.getContents() == null) {//
+//                Toast.makeText(this, "Erro na leitura", Toast.LENGTH_LONG).show();//
+//            } else {
+//                Toast.makeText(this, "Codigo: " + result.getContents(), Toast.LENGTH_LONG).show();//
+//            }
+//        } else {
+//            super.onActivityResult(requestCode, resultCode, data);
+//        }
     }
-
 }
